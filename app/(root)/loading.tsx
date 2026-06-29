c6df50b37e6cd6e4504032e67181b5f30bf84d76
Utkarsh Kumar Gupta <victusutkarsh1@gmail.com>
2026-06-29 17:23:21 +0530

    Added loading spinner

--- app/(root)/loading.tsx
+++ app/(root)/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <div className="h-8 w-8 animate-spin rounded-full border-2 border-white/10 border-t-white" />
+    <div className="flex h-full min-h-screen w-full items-center justify-center pb-64">
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-200 border-t-black" />
     </div>
   );
 }
